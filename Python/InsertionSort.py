c18dd56780fa5938453006a7d9ba9ee52368cfa3
Carolyn <whitehawk6888@yahoo.com>
2017-02-07 15:28:56 -0800

    Added Insertion sorting algorithm

--- Python/InsertionSort.py
+++ Python/InsertionSort.py
@@ -1,16 +1,16 @@
 def insertion_sort(arr):
     x = 0
-    for index in range(1, 6):
+    for index in range(1, len(arr)):
         if arr[index] <= arr[x]:
-            for count in range(0,x+1):
-                y = 0
-                if arr[count] < arr[x] and arr[count] > arr[0]:
-                    y = count
-            arr.insert(y,"hi")
+            i = 0
+            y = 0
+            while arr[i] < arr[index]:
+                i += 1
+            y = i
+            arr.insert(y,"blank")
             arr[y] = arr[index+1]
             arr.pop(index+1)
             x += 1
-            print arr
     return arr
 
-insertion_sort([8,3,1,1,7,2,4,5])
+print insertion_sort([7, 1, 2, 1, 6, 8, 54, 2])
