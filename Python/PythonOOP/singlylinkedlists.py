394fbd27123863a6f2d605cc67856320446e918f
Carolyn <whitehawk6888@yahoo.com>
2017-02-15 14:59:31 -0800

    Added Singly Linked Lists method definitions

--- Python/PythonOOP/singlylinkedlists.py
+++ Python/PythonOOP/singlylinkedlists.py
@@ -7,7 +7,6 @@ class SinglyLinkedList(object):
  def __init__(self):
   self.head = None
   self.tail = None
-  self.last = self
  def PrintAllVals(self):
      runner = self.head
      while(runner != None):
@@ -15,12 +14,17 @@ class SinglyLinkedList(object):
        runner = runner.next
      return self
  def AddBack(self, val):
-     new_node= Node(val)
-     runner = self.head
-     while(runner.next != None):
-        runner = runner.next
-     runner.next = new_node
-     return self
+     if self.head == None:
+         self.head = Node(val)
+         self.tail = self.head
+         return self
+     else:
+       runner = self.head
+       while(runner.next != None):
+         runner = runner.next
+       runner.next = Node(val)
+       self.tail = runner.next
+       return self
  def AddFront(self, val):
      new_node= Node(val)
      temp = self.head
@@ -30,23 +34,47 @@ class SinglyLinkedList(object):
  def InsertBefore(self, nextVal, val):
       new_node= Node(val)
       runner = self.head
-      while(runner.value != nextVal):
-         runner = runner.next
+      while(runner.next.value != nextVal):
+        runner = runner.next
+      temp = runner.next
       runner.next = new_node
-      runner.next.next
+      runner.next.next = temp
       return self
  def InsertAfter(self, preval, val):
-     return self
+      new_node= Node(val)
+      runner = self.head
+      while(runner.value != preval):
+        runner = runner.next
+      temp = runner.next
+      runner.next = new_node
+      runner.next.next = temp
+      return self
  def RemoveNode(self, val):
+     runner = self.head
+     while(runner.next.value != val):
+       runner = runner.next
+     runner.next = runner.next.next
      return self
  def ReverseList(self):
+     new_head = self.tail
+     while self.head != self.tail:
+         runner = self.head
+         while runner.next.next != None:
+           runner = runner.next
+         runner.next.next = runner
+         runner.next = None
+         self.tail = runner
+     self.head = new_head
      return self
 
 list = SinglyLinkedList()
-list.head = Node('Alice')
-list.head.next = Node('Chad')
-list.head.next.next = Node('Debra')
-
+list.AddBack('Alice')
+list.AddBack('Chad')
+list.AddBack('Debra')
+list.AddBack('Me')
+# list.PrintAllVals()
 # list.AddBack('Back').PrintAllVals()
 # list.AddFront('Front').PrintAllVals()
-list.InsertBefore('Chad','InBefore').PrintAllVals()
+# list.InsertAfter('Chad','After').PrintAllVals()
+# list.RemoveNode('Chad').PrintAllVals()
+list.ReverseList().PrintAllVals()
