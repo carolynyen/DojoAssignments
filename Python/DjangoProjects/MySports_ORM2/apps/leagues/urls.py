9569a1d1b64dec9f6bce07652d1807e5a02176c9
Carolyn <whitehawk6888@yahoo.com>
2017-02-21 14:01:59 -0800

    Added MySports_ORM2 level 2

--- Python/DjangoProjects/MySports_ORM2/apps/leagues/urls.py
+++ Python/DjangoProjects/MySports_ORM2/apps/leagues/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r"^$", views.index4, name="index"),
+	url(r"^$", views.index8, name="index"),
 	url(r"^make_data/", views.make_data, name="make_data"),
 ]
--- Python/DjangoProjects/MySports_ORM2/apps/leagues/views.py
+++ Python/DjangoProjects/MySports_ORM2/apps/leagues/views.py
@@ -11,83 +11,43 @@ def index(request):
 	return render(request, "leagues/index.html", context)
 
 def index1(request):
-    user = League.objects.filter(sport="Baseball")
-    context = {"leagues": user, "teams": Team.objects.all(), "players": Player.objects.all(),}
+    user = Team.objects.filter(league__name="Atlantic Soccer Conference")
+    context = {"leagues": League.objects.all(), "teams": user, "players": Player.objects.all(),}
     return render(request, "leagues/index.html", context)
 
 def index2(request):
-    user = League.objects.filter(name__contains= "Womens'")
-    context = {"leagues": user, "teams": Team.objects.all(), "players": Player.objects.all(),}
+    user = Player.objects.filter(curr_team__team_name = "Penguins", curr_team__location= "Boston")
+    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user,}
     return render(request, "leagues/index.html", context)
 
 def index3(request):
-    user = League.objects.filter(sport__contains= "Hockey")
-    context = {"leagues": user, "teams": Team.objects.all(), "players": Player.objects.all(),}
+    user = Player.objects.filter(curr_team__league__name = "International Collegiate Baseball Conference")
+    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user}
     return render(request, "leagues/index.html", context)
 
 def index4(request):
-    user = League.objects.exclude(sport="Football")
-    context = {"leagues": user, "teams": Team.objects.all(), "players": Player.objects.all(),}
+    user = Player.objects.filter(curr_team__league__name = "American Conference of Amateur Football", last_name="Lopez")
+    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user}
     return render(request, "leagues/index.html", context)
 
 def index5(request):
-    user = League.objects.filter(name__contains= "Conference")
-    context = {"leagues": user, "teams": Team.objects.all(), "players": Player.objects.all(),}
+    user = Player.objects.filter(curr_team__league__sport = "Football")
+    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user}
     return render(request, "leagues/index.html", context)
 
 def index6(request):
-    user = League.objects.filter(name__contains= "Atlantic")
-    context = {"leagues": user, "teams": Team.objects.all(), "players": Player.objects.all(),}
+    user = Team.objects.filter(curr_players__first_name = "Sophia")
+    context = {"leagues": League.objects.all(), "teams": user, "players": Player.objects.all()}
     return render(request, "leagues/index.html", context)
 
 def index7(request):
-    user = Team.objects.filter(location= "Dallas")
-    context = {"leagues": League.objects.all(), "teams": user, "players": Player.objects.all(),}
+    user = League.objects.filter(teams__curr_players__first_name = "Sophia")
+    context = {"leagues": user, "teams": Team.objects.all(), "players": Player.objects.all()}
     return render(request, "leagues/index.html", context)
 
 def index8(request):
-    user = Team.objects.filter(team_name= "Raptors")
-    context = {"leagues": League.objects.all(), "teams": user, "players": Player.objects.all(),}
-    return render(request, "leagues/index.html", context)
-
-def index9(request):
-    user = Team.objects.filter(location__contains= "City")
-    context = {"leagues": League.objects.all(), "teams": user, "players": Player.objects.all(),}
-    return render(request, "leagues/index.html", context)
-
-def index10(request):
-    user = Team.objects.filter(team_name__startswith= "T")|Team.objects.filter(location__startswith= "T")
-    context = {"leagues": League.objects.all(), "teams": user, "players": Player.objects.all(),}
-    return render(request, "leagues/index.html", context)
-
-def index11(request):
-    user = Team.objects.order_by('location')
-    context = {"leagues": League.objects.all(), "teams": user, "players": Player.objects.all(),}
-    return render(request, "leagues/index.html", context)
-
-def index12(request):
-    user = Team.objects.order_by('-team_name')
-    context = {"leagues": League.objects.all(), "teams": user, "players": Player.objects.all(),}
-    return render(request, "leagues/index.html", context)
-
-def index13(request):
-    user = Player.objects.filter(last_name="Cooper")
-    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user,}
-    return render(request, "leagues/index.html", context)
-
-def index14(request):
-    user = Player.objects.filter(first_name="Joshua")
-    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user,}
-    return render(request, "leagues/index.html", context)
-
-def index15(request):
-    user = Player.objects.filter(last_name="Cooper").exclude(first_name="Joshua")
-    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user,}
-    return render(request, "leagues/index.html", context)
-
-def index16(request):
-    user = Player.objects.filter(first_name="Alexander")|Player.objects.filter(first_name="Wyatt")
-    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user,}
+    user = Player.objects.exclude(curr_team__location = "Washinton",curr_team__team_name = "Roughriders").filter(last_name="Flores")
+    context = {"leagues": League.objects.all(), "teams": Team.objects.all(), "players": user}
     return render(request, "leagues/index.html", context)
 
 def make_data(request):
