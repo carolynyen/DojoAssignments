498ea6929851f0b7d93c6d63e0397a6df04cb156
Carolyn <whitehawk6888@yahoo.com>
2017-02-22 19:13:30 -0800

    initial commit of Dojo Secrets

---
+++ Python/DjangoProjects/DojoSecrets/apps/dojosecrets/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^register$', views.register),
+    url(r'^login$', views.login),
+    url(r'^success$', views.success),
+    url(r'^delete/(?P<id>\d+)$', views.delete),
+    url(r'^deletesecret/(?P<id>\d+)$', views.deletesecret),
+    url(r'^logout$', views.logout),
+    url(r'^postsecret$', views.addsecret),
+    url(r'^popularsecrets$', views.showpopular),
+    url(r'^.+$', views.index),
+]
---
+++ Python/DjangoProjects/DojoSecrets/apps/dojosecrets/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, HttpResponse, redirect
+from .models import User, Secret
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    request.session['success'] = False   #  resetting the logged in or registered person
+    return render(request, 'dojosecrets/index.html')
+
+def register(request):
+    if request.method == "GET":
+        return redirect('/')
+    user = User.objects.register(request.POST)
+    if 'errors' in user:
+        error = user['errors']
+        for one in error:
+            messages.error(request, one)
+        return redirect('/')
+    if user['register'] == True:
+        user = User.objects.filter(email = request.POST['email'])
+        request.session['userid'] = user[0].id
+        request.session['success'] = True
+    return redirect('/success')
+
+def success(request):
+    if request.session['success'] == False:
+        return redirect('/')
+    if 'userid' not in request.session:
+        return redirect('/')
+    user = Secret.objects.all().order_by('-id')
+    context = {'user': user[:5], 'loggeduser': User.objects.filter(id=request.session['userid'])[0]}
+    return render(request, 'dojosecrets/recentsecrets.html', context)
+
+def login(request):
+    if request.method == "GET":
+        return redirect('/')
+    user = User.objects.login(request.POST)
+    if 'errors' in user:
+        error = user['errors']
+        for one in error:
+            messages.error(request, one)
+        return redirect('/')
+    if user['login'] == True:
+        user = User.objects.filter(email = request.POST['email'])
+        request.session['userid'] = user[0].id
+        request.session['success'] = True
+    return redirect('/success')
+
+def delete(request, id):
+    if request.method == "GET":
+        return redirect('/')
+    User.objects.filter(id=id).delete()
+    return redirect('/success')
+
+
+def deletesecret(request, id):
+    if request.method == "GET":
+        return redirect('/')
+    Secret.objects.filter(id=id).delete()
+    return redirect('/success')
+
+def addsecret(request):
+    if request.method == "GET":
+        return redirect('/')
+    secret = Secret.objects.validate(request.POST)
+    if 'errors' in secret:
+        error = secret['errors']
+        for one in error:
+            messages.error(request, one)
+            return redirect('/success')
+    else:
+        Secret.objects.create(content=request.POST['secret'], creator= User.objects.get(id=request.session['userid']))
+        return redirect('/success')
+
+def showpopular(request):
+    if request.session['success'] == False:
+        return redirect('/')
+    if 'userid' not in request.session:
+        return redirect('/')
+    context = {'user': Secret.objects.all(), 'loggeduser': User.objects.filter(id=request.session['userid'])[0]}
+    return render(request, 'dojosecrets/popularsecrets.html', context)
+
+def logout(request):
+    if request.method == "GET":
+        return redirect('/')
+    request.session['success'] = False
+    del request.session['userid']
+    return redirect('/')
