d88fecd6dc389a3ab73a1fb7b2c2d1c8792be6a4
Carolyn <whitehawk6888@yahoo.com>
2017-02-24 09:00:21 -0800

    Updated UserDashboard

--- Python/DjangoProjects/UserDashboard/apps/loginreg/urls.py
+++ Python/DjangoProjects/UserDashboard/apps/loginreg/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     url(r'^$', views.index, name = "loginreg_index"),
     url(r'^register$', views.register, name="loginreg_register"),
     url(r'^login$', views.login, name="loginreg_login"),
-    url(r'^success$', views.success),
-    url(r'^delete/(?P<id>\d+)$', views.delete),
+    url(r'^success$', views.success, name="loginreg_success"),
+    url(r'^delete/(?P<id>\d+)$', views.delete, name="loginreg_delete"),
     url(r'^logout$', views.logout),
     url(r'^.+$', views.index),
 ]
