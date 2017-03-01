5040ef98f4aff9add906d7bcde993e0f9b7ab3f0
Carolyn <whitehawk6888@yahoo.com>
2017-02-28 22:23:52 -0800

    Updated UserDashboard

--- Python/DjangoProjects/UserDashboard/apps/userdashboard/models.py
+++ Python/DjangoProjects/UserDashboard/apps/userdashboard/models.py
@@ -2,5 +2,6 @@ from __future__ import unicode_literals
 import re
 from django.db import models
 from django.contrib import messages
+from ..loginreg.models import User
 
 # Create your models here.
--- Python/DjangoProjects/UserDashboard/apps/userdashboard/views.py
+++ Python/DjangoProjects/UserDashboard/apps/userdashboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from django.db.models import Count
+from django.core.urlresolvers import reverse
+from ..loginreg.models import User
 
 # Create your views here.
 def index(request):
     request.session['success'] = False   #  resetting the logged in or registered person
-    return render(request, 'loginreg:loginreg_index')
+    return redirect(reverse('loginreg:loginreg_index'))
