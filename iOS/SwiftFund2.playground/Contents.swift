8038b28f947231b27077143e2a9716e7d9dac5cd
Carolyn <whitehawk6888@yahoo.com>
2017-03-06 11:52:31 -0800

    Added SwiftFund2

--- iOS/SwiftFund2.playground/Contents.swift
+++ iOS/SwiftFund2.playground/Contents.swift
@@ -3,3 +3,27 @@
 import UIKit
 
 var str = "Hello, playground"
+var number = 2
+if number % 2 == 0 {
+    print("Number is even")
+} else {
+    print("Number is odd")
+}
+
+//for i in 1...125{
+//    print(i)
+//}
+
+for i in 1...100{
+    if !((i % 5 == 0) && (i % 3 == 0)) {
+        if (i % 3 == 0) {
+            print("Fuzz")
+        }
+        else if (i % 5 == 0){
+            print("Buzz")
+        }
+    }
+    else {
+        print("FizzBuzz")
+    }
+}
