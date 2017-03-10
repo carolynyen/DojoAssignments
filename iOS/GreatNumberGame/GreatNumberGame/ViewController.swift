3e7be1b95cabc84383517faf4a603b884a5ce1db
Carolyn <whitehawk6888@yahoo.com>
2017-03-09 18:10:22 -0800

    Updated GreatNumberGame

--- iOS/GreatNumberGame/GreatNumberGame/ViewController.swift
+++ iOS/GreatNumberGame/GreatNumberGame/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     var guessnum = 0
     
     @IBOutlet weak var inputnumber: UITextField!
-    @IBOutlet weak var guess: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,18 +21,15 @@ class ViewController: UIViewController {
 
     @IBAction func submit(_ sender: UIButton) {
         let alert = UIAlertController(title: "Incorrect", message: "message", preferredStyle: UIAlertControllerStyle.alert)
-        if let temp = inputnumber.text {
-            if temp.characters.count == 0 {
-                guessnum = 0
-            }
-            else {
-                guessnum = Int(temp)!
-            }
+        if let temp = Int(inputnumber.text!) {
+                guessnum = temp
+        }
+        else {
+            guessnum = 0
         }
         var status = true
-        guess.text = String(random)
         if guessnum == 0  {
-            alert.message = "Can't be empty"
+            alert.message = "Invalid input!"
             status = false
         }
         else if guessnum < random {
@@ -55,10 +51,13 @@ class ViewController: UIViewController {
             alert.view.backgroundColor = UIColor.green
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
+            resetgame()
         }
         inputnumber.text = nil
     }
-    
+    func resetgame(){
+        random = Int(arc4random_uniform(100)+1)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
