1ce199b5167fc66d1a3bbac47bb044269aac245d
Carolyn <whitehawk6888@yahoo.com>
2017-03-09 22:54:12 -0800

    Updated GreatNumberGame

---
+++ iOS/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Carolyn Yen on 3/9/17.
+//  Copyright © 2017 Carolyn Yen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
